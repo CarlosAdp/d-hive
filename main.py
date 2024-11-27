24aceaf3d615459281cfec077b19c00459a8e72c
Carlos Pinto <carlos.adpinto@gmail.com>
2024-11-27 04:40:15 -0300

    Add linter

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ import discord
 
 load_dotenv()
 
+
 class HiveDJClient(discord.Client):
     async def on_ready(self):
-        print(f"Logged on as {self.user}!")
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.lower().startswith('hey dj'):
+            await message.channel.send(
+                'Hey! What can I do for you? Differently from other DJs, you '
+                'can ask me to play music, and I will play it for you!')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 hive_dj_client = HiveDJClient(intents=intents)
-hive_dj_client.run(os.getenv("DISCORD_BOT_TOKEN"))
+hive_dj_client.run(os.getenv('DISCORD_BOT_TOKEN'))
